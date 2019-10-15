f5058bcfaec9b7349396705c4eaa4f2b065451c0
xengus <serjiox13@gmail.com>
2019-10-15 22:45:04 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function zeros(expression) {
-  // your solution
+  const expr= expression.split("*");
+  let numberTwo = 0;
+  let numberFive = 0;
+	for (let i = 0; i < expr.length; i++) {
+		const key = (expr[i][expr[i].length - 2] == "!") ? 2 : 1;
+		expr[i] = parseInt(expr[i]);
+		for (let j = expr[i]; j > 1; j -= key) {
+			let result = j;
+			while (result % 5 == 0) {
+				numberFive++;
+				result /= 5;
+			}
+			while (result % 2 == 0) {
+				numberTwo++;
+				result /= 2;
+			}
+		}
+	}
+	return Math.min(numberTwo, numberFive); 
 }
